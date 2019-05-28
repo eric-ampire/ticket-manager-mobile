a39015c54b61a1e298b0820365c5fea58f9f396a
Eric Ampire <ericampire.top@gmail.com>
2019-05-28 20:55:18 +0200

    [ADD] Lieu Dao

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/model/dao/LieuDao.kt
@@ -0,0 +1,23 @@
+package org.pbreakers.mobile.getticket.model.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import org.pbreakers.mobile.getticket.model.entity.Lieu
+
+@Dao
+interface LieuDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun add(vararg data: Lieu)
+
+    @Query("SELECT * FROM Lieu WHERE id=:id")
+    fun findById(id: Int): LiveData<Lieu>
+
+    @Query("SELECT * FROM Lieu")
+    fun findAll(): LiveData<List<Lieu>>
+
+    @Update
+    fun update(vararg data: Lieu)
+
+    @Delete
+    fun remove(vararg data: Lieu)
+}
