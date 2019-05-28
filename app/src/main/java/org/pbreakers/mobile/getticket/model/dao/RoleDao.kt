7754384e2b2a1a573ee5ba5d9221a46160bf0223
Eric Ampire <ericampire.top@gmail.com>
2019-05-28 20:58:15 +0200

    [ADD] Role Dao

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/model/dao/RoleDao.kt
@@ -0,0 +1,23 @@
+package org.pbreakers.mobile.getticket.model.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import org.pbreakers.mobile.getticket.model.entity.Role
+
+@Dao
+interface RoleDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun add(vararg data: Role)
+
+    @Query("SELECT * FROM Role WHERE id=:id")
+    fun findById(id: Int): LiveData<Role>
+
+    @Query("SELECT * FROM Role")
+    fun findAll(): LiveData<List<Role>>
+
+    @Update
+    fun update(vararg data: Role)
+
+    @Delete
+    fun remove(vararg data: Role)
+}
