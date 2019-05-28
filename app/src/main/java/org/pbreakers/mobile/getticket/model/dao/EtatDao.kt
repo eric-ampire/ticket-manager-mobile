9616be89e60fd597344b6c569c07bea9fe6e977a
Eric Ampire <ericampire.top@gmail.com>
2019-05-28 20:52:00 +0200

    [ADD] Etat Dao

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/model/dao/EtatDao.kt
@@ -0,0 +1,23 @@
+package org.pbreakers.mobile.getticket.model.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import org.pbreakers.mobile.getticket.model.entity.Etat
+
+@Dao
+interface EtatDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun add(vararg data: Etat)
+
+    @Query("SELECT * FROM Etat WHERE id=:id")
+    fun findById(id: Int): LiveData<Etat>
+
+    @Query("SELECT * FROM Etat")
+    fun findAll(): LiveData<List<Etat>>
+
+    @Update
+    fun update(vararg data: Etat)
+
+    @Delete
+    fun remove(vararg data: Etat)
+}
