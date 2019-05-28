ccfb60831e15fee436d02f443fb948044784243d
Eric Ampire <ericampire.top@gmail.com>
2019-05-28 20:47:54 +0200

    [ADD] Etat Entity

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/model/entity/Etat.kt
@@ -0,0 +1,13 @@
+package org.pbreakers.mobile.getticket.model.entity
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Entity
+@Parcelize
+class Etat(
+    @PrimaryKey val id: Long,
+    val nom: String
+) : Parcelable
