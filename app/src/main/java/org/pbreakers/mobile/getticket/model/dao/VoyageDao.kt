e69da66506979bca2d366ffbeb41c4b47685ee82
Eric Ampire <ericampire.top@gmail.com>
2019-05-28 21:18:11 +0200

    [ADD] Voyage Dao

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/model/dao/VoyageDao.kt
@@ -0,0 +1,23 @@
+package org.pbreakers.mobile.getticket.model.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import org.pbreakers.mobile.getticket.model.entity.Voyage
+
+@Dao
+interface VoyageDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun add(vararg data: Voyage)
+
+    @Query("SELECT * FROM Voyage WHERE id=:id")
+    fun findById(id: Int): LiveData<Voyage>
+
+    @Query("SELECT * FROM Voyage")
+    fun findAll(): LiveData<List<Voyage>>
+
+    @Update
+    fun update(vararg data: Voyage)
+
+    @Delete
+    fun remove(vararg data: Voyage)
+}
