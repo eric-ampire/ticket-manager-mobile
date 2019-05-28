a1bc10efd6d9e0bdfe8f15e45a96b3f20595dd8f
Eric Ampire <ericampire.top@gmail.com>
2019-05-28 21:25:45 +0200

    [ADD] PoindArret Dao

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/model/dao/PointArretDao.kt
@@ -0,0 +1,23 @@
+package org.pbreakers.mobile.getticket.model.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import org.pbreakers.mobile.getticket.model.entity.PointArret
+
+@Dao
+interface PointArretDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun add(vararg data: PointArret)
+
+    @Query("SELECT * FROM PointArret WHERE id=:id")
+    fun findById(id: Int): LiveData<PointArret>
+
+    @Query("SELECT * FROM PointArret")
+    fun findAll(): LiveData<List<PointArret>>
+
+    @Update
+    fun update(vararg data: PointArret)
+
+    @Delete
+    fun remove(vararg data: PointArret)
+}
