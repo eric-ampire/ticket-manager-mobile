71f214074db5cc9f2d726c719566d7fa64b5db7c
Eric Ampire <ericampire.top@gmail.com>
2019-05-28 21:08:41 +0200

    [ADD] BilletDao

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/model/dao/BilletDao.kt
@@ -0,0 +1,23 @@
+package org.pbreakers.mobile.getticket.model.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import org.pbreakers.mobile.getticket.model.entity.Billet
+
+@Dao
+interface BilletDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun add(vararg data: Billet)
+
+    @Query("SELECT * FROM Billet WHERE id=:id")
+    fun findById(id: Int): LiveData<Billet>
+
+    @Query("SELECT * FROM Billet")
+    fun findAll(): LiveData<List<Billet>>
+
+    @Update
+    fun update(vararg data: Billet)
+
+    @Delete
+    fun remove(vararg data: Billet)
+}
