ae11a85960ef549ea267b1ade787b2d55525f045
Eric Ampire <ericampire.top@gmail.com>
2019-05-28 21:00:26 +0200

    [ADD] Transit Dao

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/model/dao/TransitDao.kt
@@ -0,0 +1,23 @@
+package org.pbreakers.mobile.getticket.model.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import org.pbreakers.mobile.getticket.model.entity.Transit
+
+@Dao
+interface TransitDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun add(vararg data: Transit)
+
+    @Query("SELECT * FROM Transit WHERE id=:id")
+    fun findById(id: Int): LiveData<Transit>
+
+    @Query("SELECT * FROM Transit")
+    fun findAll(): LiveData<List<Transit>>
+
+    @Update
+    fun update(vararg data: Transit)
+
+    @Delete
+    fun remove(vararg data: Transit)
+}
