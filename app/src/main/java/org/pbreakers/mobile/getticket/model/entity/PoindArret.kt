8cc557f9f034e9751db51432cca8522e71fb3bcc
Eric Ampire <ericampire.top@gmail.com>
2019-05-28 21:24:00 +0200

    [ADD] PoindArret Entity

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/model/entity/PoindArret.kt
@@ -0,0 +1,22 @@
+package org.pbreakers.mobile.getticket.model.entity
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Entity(
+    indices = [Index("idLieu", "idVoyage")],
+    foreignKeys = [
+        ForeignKey(entity = Lieu::class, parentColumns = ["id"], childColumns = ["idLieu"]),
+        ForeignKey(entity = Voyage::class, parentColumns = ["id"], childColumns = ["idVoyage"])
+    ]
+)
+@Parcelize
+class PoindArret(
+    @PrimaryKey val id: Long,
+    val idLieu: Long,
+    val idVoyage: Long
+) : Parcelable
