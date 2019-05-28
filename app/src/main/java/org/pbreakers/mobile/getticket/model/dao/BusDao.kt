f40ce7c855d2d53a3b6a52c7d0eaa032041ce4b7
Eric Ampire <ericampire.top@gmail.com>
2019-05-28 20:50:30 +0200

    [ADD] Bus Dao

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/model/dao/BusDao.kt
@@ -0,0 +1,23 @@
+package org.pbreakers.mobile.getticket.model.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import org.pbreakers.mobile.getticket.model.entity.Bus
+
+@Dao
+interface BusDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun add(vararg data: Bus)
+
+    @Query("SELECT * FROM Bus WHERE id=:id")
+    fun findById(id: Int): LiveData<Bus>
+
+    @Query("SELECT * FROM Bus")
+    fun findAll(): LiveData<List<Bus>>
+
+    @Update
+    fun update(vararg data: Bus)
+
+    @Delete
+    fun remove(vararg data: Bus)
+}
