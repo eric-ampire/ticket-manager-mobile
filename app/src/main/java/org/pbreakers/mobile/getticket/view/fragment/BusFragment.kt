eb7d57834cc1e4315c32844fea09a920b244fdd4
Eric Ampire <ericampire.top@gmail.com>
2019-05-29 23:28:11 +0200

    [UPDATE] Connect BusFragment To viewModel

--- app/src/main/java/org/pbreakers/mobile/getticket/view/fragment/BusFragment.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/view/fragment/BusFragment.kt
@@ -6,17 +6,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil.inflate
+import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.get
 
 import org.pbreakers.mobile.getticket.R
+import org.pbreakers.mobile.getticket.databinding.FragmentBusBinding
+import org.pbreakers.mobile.getticket.viewmodel.BusViewModel
 
 
 class BusFragment : Fragment() {
+
+    private val busViewModel by lazy {
+        ViewModelProviders.of(this).get<BusViewModel>()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bus, container, false)
+        val binding = inflate<FragmentBusBinding>(inflater, R.layout.fragment_bus, container, false)
+        binding.viewModel = busViewModel
+
+        busViewModel.init()
+
+        return binding.root
     }
 }
