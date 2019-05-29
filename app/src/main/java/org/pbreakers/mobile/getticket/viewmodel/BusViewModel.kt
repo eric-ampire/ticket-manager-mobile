a644bc43ca9afd35fe9aea2650d8178e9aeb95b7
Eric Ampire <ericampire.top@gmail.com>
2019-05-29 23:00:06 +0200

    [ADD] BusViewModel.kt

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/viewmodel/BusViewModel.kt
@@ -0,0 +1,7 @@
+package org.pbreakers.mobile.getticket.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class BusViewModel(val app: Application) : AndroidViewModel(app) {
+}
