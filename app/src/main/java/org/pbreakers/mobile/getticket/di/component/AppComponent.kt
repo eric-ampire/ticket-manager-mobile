39c303cefaf1c8d28e973a8d7cbaa180fb997858
Eric Ampire <ericampire.top@gmail.com>
2019-05-29 10:01:40 +0200

    [UPDATE] Dependencies for AuthViewModel

--- app/src/main/java/org/pbreakers/mobile/getticket/di/component/AppComponent.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/di/component/AppComponent.kt
@@ -2,7 +2,9 @@ package org.pbreakers.mobile.getticket.di.component
 
 import dagger.Component
 import org.pbreakers.mobile.getticket.di.module.*
+import org.pbreakers.mobile.getticket.model.dao.UtilisateurDao
 import org.pbreakers.mobile.getticket.model.entity.Agence
+import org.pbreakers.mobile.getticket.viewmodel.AuthViewModel
 import javax.inject.Singleton
 
 @Singleton
@@ -14,7 +16,8 @@ import javax.inject.Singleton
 ])
 interface AppComponent {
     // Field injection
-
+    fun inject(authViewModel: AuthViewModel)
 
     // Constructor injection
+    fun utilisateurDao(): UtilisateurDao
 }
