577fa2bdef5894541149cefc0722a1fb9e1156ec
Eric Ampire <ericampire.top@gmail.com>
2019-05-29 09:38:26 +0200

    [ADD] Auth Activity

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/view/activity/AuthActivity.kt
@@ -0,0 +1,13 @@
+package org.pbreakers.mobile.getticket.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.pbreakers.mobile.getticket.R
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
