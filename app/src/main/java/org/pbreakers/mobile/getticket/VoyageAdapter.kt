51396f381a98154a4789acce403236322d8f4cdb
Eric Ampire <ericampire.top@gmail.com>
2019-05-29 22:13:56 +0200

    [ADD] VoyageAdapter.kt

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/VoyageAdapter.kt
@@ -0,0 +1,18 @@
+package org.pbreakers.mobile.getticket
+
+import androidx.recyclerview.widget.DiffUtil
+import org.pbreakers.mobile.getticket.model.entity.Voyage
+
+class VoyageAdapter {
+
+
+
+
+
+    companion object {
+        val COMPARATOR = object : DiffUtil.ItemCallback<Voyage>() {
+            override fun areItemsTheSame(oldItem: Voyage, newItem: Voyage): Boolean = newItem.id == oldItem.id
+            override fun areContentsTheSame(oldItem: Voyage, newItem: Voyage): Boolean = newItem === oldItem
+        }
+    }
+}
