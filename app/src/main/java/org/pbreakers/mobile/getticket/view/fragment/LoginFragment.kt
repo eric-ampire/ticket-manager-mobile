6d8a7130ebe13091c9a0ca099e4f33a935ca536c
Eric Ampire <ericampire.top@gmail.com>
2019-05-29 12:13:53 +0200

    [UPDATE] Support databinding

--- app/src/main/java/org/pbreakers/mobile/getticket/view/fragment/LoginFragment.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/view/fragment/LoginFragment.kt
@@ -2,21 +2,32 @@ package org.pbreakers.mobile.getticket.view.fragment
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.inflate
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import org.pbreakers.mobile.getticket.R
+import org.pbreakers.mobile.getticket.databinding.FragmentLoginBinding
+import org.pbreakers.mobile.getticket.viewmodel.AuthViewModel
 
 
 class LoginFragment : Fragment() {
+
+    private val viewModel by lazy {
+        ViewModelProviders.of(this).get(AuthViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+        val binding = inflate<FragmentLoginBinding>(inflater, R.layout.fragment_login, container, false)
+        binding.viewModel = viewModel
+
+        return binding.root
     }
 }
