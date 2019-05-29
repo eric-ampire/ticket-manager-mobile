9f10f2597cc5be2d6c9d796a4e2398f67f1be41f
Eric Ampire <ericampire.top@gmail.com>
2019-05-29 21:40:56 +0200

    [UPDATE] Provide Field dependencies

--- app/src/main/java/org/pbreakers/mobile/getticket/di/component/AppComponent.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/di/component/AppComponent.kt
@@ -4,7 +4,10 @@ import dagger.Component
 import org.pbreakers.mobile.getticket.di.module.*
 import org.pbreakers.mobile.getticket.model.dao.UtilisateurDao
 import org.pbreakers.mobile.getticket.model.entity.Agence
+import org.pbreakers.mobile.getticket.view.activity.SplashActivity
 import org.pbreakers.mobile.getticket.viewmodel.AuthViewModel
+import org.pbreakers.mobile.getticket.util.Session
+import org.pbreakers.mobile.getticket.view.activity.MainActivity
 import javax.inject.Singleton
 
 @Singleton
@@ -17,6 +20,11 @@ import javax.inject.Singleton
 interface AppComponent {
     // Field injection
     fun inject(authViewModel: AuthViewModel)
+    fun inject(splashActivity: SplashActivity)
+    fun inject(companion: Session)
+    fun inject(mainActivity: MainActivity)
+
+
 
     // Constructor injection
     fun utilisateurDao(): UtilisateurDao
