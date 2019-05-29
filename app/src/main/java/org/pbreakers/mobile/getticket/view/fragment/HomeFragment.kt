caae06dc34e4fcb647302ae20ad5a19e940f9860
Eric Ampire <ericampire.top@gmail.com>
2019-05-29 22:54:17 +0200

    [UPDATE] Connect viewModel to homeFragment

--- app/src/main/java/org/pbreakers/mobile/getticket/view/fragment/HomeFragment.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/view/fragment/HomeFragment.kt
@@ -2,21 +2,32 @@ package org.pbreakers.mobile.getticket.view.fragment
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.databinding.DataBindingUtil.inflate
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.get
 import org.pbreakers.mobile.getticket.R
+import org.pbreakers.mobile.getticket.databinding.FragmentHomeBinding
+import org.pbreakers.mobile.getticket.viewmodel.HomeViewModel
 
 
 class HomeFragment : Fragment() {
+
+    private val homeViewModel by lazy {
+        ViewModelProviders.of(this).get<HomeViewModel>()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val binding = inflate<FragmentHomeBinding>(inflater, R.layout.fragment_home, container, false)
+        binding.viewModel = homeViewModel
+
+        return binding.root
     }
 }
