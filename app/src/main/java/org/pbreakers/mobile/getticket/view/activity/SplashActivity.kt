abff0ad19eff3d434e1e027ed435ba3d29692b65
Eric Ampire <ericampire.top@gmail.com>
2019-05-29 10:53:09 +0200

    [UPDATE] Manage session

--- app/src/main/java/org/pbreakers/mobile/getticket/view/activity/SplashActivity.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/view/activity/SplashActivity.kt
@@ -1,13 +1,33 @@
 package org.pbreakers.mobile.getticket.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.jetbrains.anko.startActivity
 import org.pbreakers.mobile.getticket.R
+import org.pbreakers.mobile.getticket.app.App
+import org.pbreakers.mobile.getticket.util.Session
+import javax.inject.Inject
 
 class SplashActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var session: Session
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val app = application as App
+        app.appComponent.inject(this)
+
+        session.getCurrentUser {
+            if (it == null) {
+                startActivity<AuthActivity>()
+            } else {
+                startActivity<MainActivity>()
+            }
+
+            finish()
+        }
     }
 }
