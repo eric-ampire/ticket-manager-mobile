c8429fc3d2ced720249895b2a5aa7c98955a8194
Eric Ampire <ericampire.top@gmail.com>
2019-05-30 11:58:31 +0200

    [ADD] CustomViewHolder.kt

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/adapter/CustomViewHolder.kt
@@ -0,0 +1,7 @@
+package org.pbreakers.mobile.getticket.adapter
+
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+class CustomViewHolder(val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root)
+
