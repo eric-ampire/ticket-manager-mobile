dfcea15dfc41346ca52388dcc0d10b7f700da4d6
Eric Ampire <ericampire.top@gmail.com>
2019-05-30 07:14:35 +0200

    [ADD] Tools functions

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/util/Tools.kt
@@ -0,0 +1,331 @@
+package org.pbreakers.mobile.eduquelib.util
+
+import android.app.Activity
+import android.app.Dialog
+import android.content.*
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import android.util.TypedValue
+import android.view.Menu
+import android.view.View
+import android.view.WindowManager
+import android.webkit.URLUtil
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.core.widget.NestedScrollView
+import org.pbreakers.mobile.eduquelib.R
+import org.pbreakers.mobile.getticket.R
+
+import java.net.URI
+import java.net.URISyntaxException
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
+object Tools {
+
+    val screenWidth: Int
+        get() = Resources.getSystem().displayMetrics.widthPixels
+
+    val screenHeight: Int
+        get() = Resources.getSystem().displayMetrics.heightPixels
+
+    /**
+     * For device info parameters
+     */
+    val deviceName: String
+        get() {
+            val manufacturer = Build.MANUFACTURER
+            val model = Build.MODEL
+            return if (model.startsWith(manufacturer)) {
+                model
+            } else {
+                "$manufacturer $model"
+            }
+        }
+
+    val androidVersion: String
+        get() = Build.VERSION.RELEASE + ""
+
+    fun setSystemBarColor(act: Activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window = act.window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = act.resources.getColor(R.color.colorPrimaryDark)
+        }
+    }
+
+    fun setSystemBarColor(act: Activity, @ColorRes color: Int) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window = act.window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = act.resources.getColor(color)
+        }
+    }
+
+    fun setSystemBarColorInt(act: Activity, @ColorInt color: Int) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window = act.window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = color
+        }
+    }
+
+    fun setSystemBarColorDialog(act: Context, dialog: Dialog, @ColorRes color: Int) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window = dialog.window
+            window!!.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = act.resources.getColor(color)
+        }
+    }
+
+    fun setSystemBarLight(act: Activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val view = act.findViewById<View>(android.R.id.content)
+            var flags = view.systemUiVisibility
+            flags = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            view.systemUiVisibility = flags
+        }
+    }
+
+    fun setSystemBarLightDialog(dialog: Dialog) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val view = dialog.findViewById<View>(android.R.id.content)
+            var flags = view.systemUiVisibility
+            flags = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            view.systemUiVisibility = flags
+        }
+    }
+
+    fun clearSystemBarLight(act: Activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val window = act.window
+            window.statusBarColor = ContextCompat.getColor(act, R.color.colorPrimaryDark)
+        }
+    }
+
+    /**
+     * Making notification bar transparent
+     */
+    fun setSystemBarTransparent(act: Activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window = act.window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = Color.TRANSPARENT
+        }
+    }
+
+    fun getFormattedDateShort(dateTime: Long?): String {
+        val newFormat = SimpleDateFormat("MMM dd, yyyy", Locale.CANADA)
+        return newFormat.format(Date(dateTime!!))
+    }
+
+    fun getFormattedDateSimple(dateTime: Long?): String {
+        val newFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.CANADA)
+        return newFormat.format(Date(dateTime!!))
+    }
+
+    fun getFormattedDateEvent(dateTime: Long?): String {
+        val newFormat = SimpleDateFormat("EEE, MMM dd yyyy", Locale.CANADA)
+        return newFormat.format(Date(dateTime!!))
+    }
+
+    fun getFormattedTimeEvent(time: Long?): String {
+        val newFormat = SimpleDateFormat("h:mm a", Locale.CANADA)
+        return newFormat.format(Date(time!!))
+    }
+
+    fun getEmailFromName(name: String?): String? {
+        return if (name != null && name != "") {
+            name.replace(" ".toRegex(), ".").toLowerCase() + "@mail.com"
+        } else name
+    }
+
+    fun dpToPx(c: Context, dp: Int): Int {
+        val r = c.resources
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp.toFloat(), r.displayMetrics))
+    }
+
+
+    fun copyToClipboard(context: Context, data: String) {
+        val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText("clipboard", data)
+        clipboard.primaryClip = clip
+        Toast.makeText(context, "Text copied to clipboard", Toast.LENGTH_SHORT).show()
+    }
+
+    fun nestedScrollTo(nested: NestedScrollView, targetView: View) {
+        nested.post(Runnable { nested.scrollTo(500, targetView.bottom) })
+    }
+
+    fun dip2px(context: Context, dpValue: Float): Int {
+        val scale = context.resources.displayMetrics.density
+        return (dpValue * scale + 0.5f).toInt()
+    }
+
+    fun px2dip(context: Context, pxValue: Float): Int {
+        val scale = context.resources.displayMetrics.density
+        return (pxValue / scale + 0.5f).toInt()
+    }
+
+    fun toggleArrow(view: View): Boolean = if (view.rotation == 0f) {
+        view.animate().setDuration(200).rotation(180f)
+        true
+    } else {
+        view.animate().setDuration(200).rotation(0f)
+        false
+    }
+
+    @JvmOverloads
+    fun toggleArrow(show: Boolean, view: View, delay: Boolean = true): Boolean = if (show) {
+        view.animate().setDuration((if (delay) 200 else 0).toLong()).rotation(180f)
+        true
+    } else {
+        view.animate().setDuration((if (delay) 200 else 0).toLong()).rotation(0f)
+        false
+    }
+
+    fun changeNavigateionIconColor(toolbar: Toolbar, @ColorInt color: Int) {
+        val drawable = toolbar.navigationIcon
+        drawable?.mutate()
+        drawable?.setColorFilter(color, PorterDuff.Mode.SRC_ATOP)
+    }
+
+    fun changeMenuIconColor(menu: Menu, @ColorInt color: Int) {
+        for (i in 0 until menu.size()) {
+            val drawable = menu.getItem(i).icon ?: continue
+            drawable.mutate()
+            drawable.setColorFilter(color, PorterDuff.Mode.SRC_ATOP)
+        }
+    }
+
+    fun changeOverflowMenuIconColor(toolbar: Toolbar, @ColorInt color: Int) {
+        try {
+            val drawable = toolbar.overflowIcon
+            drawable?.mutate()
+            drawable?.setColorFilter(color, PorterDuff.Mode.SRC_ATOP)
+        } catch (e: Exception) {
+        }
+
+    }
+
+    fun insertPeriodically(text: String, insert: String, period: Int): String {
+        val builder = StringBuilder(text.length + insert.length * (text.length / period) + 1)
+        var index = 0
+        var prefix = ""
+        while (index < text.length) {
+            builder.append(prefix)
+            prefix = insert
+            builder.append(text.substring(index, Math.min(index + period, text.length)))
+            index += period
+        }
+        return builder.toString()
+    }
+
+
+    fun rateAction(activity: Activity) {
+        val uri = Uri.parse("market://details?id=" + activity.packageName)
+        val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+        try {
+            activity.startActivity(goToMarket)
+        } catch (e: ActivityNotFoundException) {
+            activity.startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=" + activity.packageName)
+                )
+            )
+        }
+
+    }
+
+    fun getVersionCode(ctx: Context): Int {
+        return try {
+            val manager = ctx.packageManager
+            val info = manager.getPackageInfo(ctx.packageName, 0)
+            info.versionCode
+        } catch (e: PackageManager.NameNotFoundException) {
+            -1
+        }
+
+    }
+
+    
+
+    fun getDeviceID(context: Context): String? {
+        var deviceID: String? = Build.SERIAL
+        if (deviceID == null || deviceID.trim { it <= ' ' }.isEmpty() || deviceID == "unknown") {
+            try {
+                deviceID = Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
+            } catch (e: Exception) {
+            }
+
+        }
+        return deviceID
+    }
+
+    fun getFormattedDateOnly(dateTime: Long?): String {
+        val newFormat = SimpleDateFormat("dd MMM yy", Locale.CANADA)
+        return newFormat.format(Date(dateTime!!))
+    }
+
+    fun directLinkToBrowser(activity: Activity, url: String) {
+        try {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            activity.startActivity(intent)
+        } catch (e: Exception) {
+            Toast.makeText(activity, "Ops, Cannot open url", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    private fun appendQuery(uri: String, appendQuery: String): String {
+        try {
+            val oldUri = URI(uri)
+            var newQuery: String? = oldUri.query
+            if (newQuery == null) {
+                newQuery = appendQuery
+            } else {
+                newQuery += "&$appendQuery"
+            }
+            val newUri = URI(
+                oldUri.scheme,
+                oldUri.authority,
+                oldUri.path, newQuery, oldUri.fragment
+            )
+            return newUri.toString()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return uri
+        }
+
+    }
+
+    fun getHostName(url: String): String {
+        return try {
+            val uri = URI(url)
+            var newUrl = uri.host
+            if (!newUrl.startsWith("www.")) newUrl = "www.$newUrl"
+            newUrl
+        } catch (e: URISyntaxException) {
+            e.printStackTrace()
+            url
+        }
+
+    }
+}
