7eeb14512ffbf407ff17d3973c3944af5c8ceee5
Eric Ampire <ericampire.top@gmail.com>
2019-05-30 22:54:51 +0200

    [ADD] DetailVoyageFragment.kt

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/view/fragment/DetailVoyageFragment.kt
@@ -0,0 +1,39 @@
+package org.pbreakers.mobile.getticket.view.fragment
+
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+
+import org.pbreakers.mobile.getticket.R
+
+
+class DetailVoyageFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_voyage_detail, container, false)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        return dialog
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+
+        super.onDismiss(dialog)
+        (activity as AppCompatActivity).supportActionBar?.show()
+    }
+}
