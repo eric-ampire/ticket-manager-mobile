34982d82cd917bdcb759efb3f2b658017d4bb9a8
Eric Ampire <ericampire.top@gmail.com>
2019-05-30 22:43:31 +0200

    [UPDATE] Support PagedList

--- app/src/main/java/org/pbreakers/mobile/getticket/model/dao/BusDao.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/model/dao/BusDao.kt
@@ -1,6 +1,7 @@
 package org.pbreakers.mobile.getticket.model.dao
 
 import androidx.lifecycle.LiveData
+import androidx.paging.DataSource
 import androidx.room.*
 import org.pbreakers.mobile.getticket.model.entity.Bus
 
@@ -13,7 +14,7 @@ interface BusDao {
     fun findById(id: Int): LiveData<Bus>
 
     @Query("SELECT * FROM Bus")
-    fun findAll(): LiveData<List<Bus>>
+    fun findAll(): DataSource.Factory<Int, Bus>
 
     @Update
     fun update(vararg data: Bus)
