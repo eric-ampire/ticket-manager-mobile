d2e744896c58ffa2b99d4f8fbda94f0d02356d51
Eric Ampire <ericampire.top@gmail.com>
2019-05-30 22:55:49 +0200

    [UPDATE] ViewModel Logic

--- app/src/main/java/org/pbreakers/mobile/getticket/viewmodel/BusViewModel.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/viewmodel/BusViewModel.kt
@@ -1,7 +1,55 @@
 package org.pbreakers.mobile.getticket.viewmodel
 
 import android.app.Application
+import android.app.Dialog
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.AndroidViewModel
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import org.pbreakers.mobile.eduquelib.adapter.OnItemClickListener
+import org.pbreakers.mobile.getticket.R
+import org.pbreakers.mobile.getticket.adapter.BusAdapter
+import org.pbreakers.mobile.getticket.app.App
+import org.pbreakers.mobile.getticket.model.entity.Bus
+import org.pbreakers.mobile.getticket.model.repository.BusRepository
+import org.pbreakers.mobile.getticket.view.fragment.BusDetailFragment
+import javax.inject.Inject
 
-class BusViewModel(val app: Application) : AndroidViewModel(app) {
+class BusViewModel(val app: Application) : AndroidViewModel(app), OnItemClickListener<Bus> {
+
+    @Inject lateinit var repository: BusRepository
+
+    var busAdapter = BusAdapter(this)
+    private lateinit var fragment: Fragment
+
+    init {
+        val application = app as App
+        application.appComponent.inject(this)
+    }
+
+    fun init(fragment: Fragment) {
+
+        this.fragment = fragment
+
+        val config = PagedList.Config.Builder()
+            .setPageSize(3)
+            .build()
+        val data = LivePagedListBuilder(repository.findAll(), config).build()
+
+        data.observeForever {
+            busAdapter.submitList(it)
+        }
+    }
+
+    override fun onClick(view: View, item: Bus, position: Int) {
+        val busDetailFragment = BusDetailFragment().apply {
+            this.bus = item
+        }
+
+        val hostActivity = fragment.activity as AppCompatActivity
+        busDetailFragment.show(hostActivity.supportFragmentManager, busDetailFragment.tag)
+    }
 }
--- app/src/main/java/org/pbreakers/mobile/getticket/viewmodel/HomeViewModel.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/viewmodel/HomeViewModel.kt
@@ -2,6 +2,33 @@ package org.pbreakers.mobile.getticket.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import org.pbreakers.mobile.getticket.adapter.VoyageAdapter
+import org.pbreakers.mobile.getticket.app.App
+import org.pbreakers.mobile.getticket.model.repository.VoyageRepository
+import javax.inject.Inject
 
 class HomeViewModel(val app: Application) : AndroidViewModel(app) {
+
+    lateinit var adapter: VoyageAdapter
+    @Inject lateinit var repository: VoyageRepository
+
+    init {
+        val application = app as App
+        application.appComponent.inject(this)
+    }
+
+    fun init() {
+
+        val config = PagedList.Config.Builder()
+            .setPageSize(3)
+            .build()
+
+        val data = LivePagedListBuilder(repository.findAll(), config).build()
+
+        data.observeForever {
+            adapter.submitList(it)
+        }
+    }
 }
