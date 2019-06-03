274729190ee0be7ad427a6616d5af589227573c9
Eric Ampire <ericampire.top@gmail.com>
2019-06-03 09:17:17 +0200

    [ADD] Setting fragment

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/view/fragment/SettingFragment.kt
@@ -0,0 +1,21 @@
+package org.pbreakers.mobile.getticket.view.fragment
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import org.pbreakers.mobile.getticket.R
+
+class SettingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
+}
