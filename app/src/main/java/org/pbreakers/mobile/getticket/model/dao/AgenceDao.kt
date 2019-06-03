185dfa40af43e0b5fc448e91586d55eff23bcea5
Eric Ampire <ericampire.top@gmail.com>
2019-06-03 22:09:30 +0200

    [UPDATE] Migration to RxJava

--- app/src/main/java/org/pbreakers/mobile/getticket/model/dao/AgenceDao.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/model/dao/AgenceDao.kt
@@ -2,22 +2,26 @@ package org.pbreakers.mobile.getticket.model.dao
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
+import io.reactivex.Completable
+import io.reactivex.Maybe
+import io.reactivex.Single
+import io.reactivex.SingleObserver
 import org.pbreakers.mobile.getticket.model.entity.Agence
 
 @Dao
 interface AgenceDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun add(vararg data: Agence)
+    fun add(vararg data: Agence): Completable
 
     @Query("SELECT * FROM Agence WHERE idAgence=:id")
-    fun findById(id: Int): LiveData<Agence>
+    fun findById(id: Int): Maybe<Agence>
 
     @Query("SELECT * FROM Agence")
-    fun findAll(): LiveData<List<Agence>>
+    fun findAll(): Maybe<List<Agence>>
 
     @Update
-    fun update(vararg data: Agence)
+    fun update(vararg data: Agence): Single<Int>
 
     @Delete
-    fun remove(vararg data: Agence)
+    fun remove(vararg data: Agence): Single<Int>
 }
