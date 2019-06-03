bf18a09b794f934d99712986b308c75361f9a860
Eric Ampire <ericampire.top@gmail.com>
2019-06-03 13:27:26 +0200

    [UPDATE] Migration RxAndroid

--- app/src/main/java/org/pbreakers/mobile/getticket/model/dao/BusDao.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/model/dao/BusDao.kt
@@ -1,24 +1,25 @@
 package org.pbreakers.mobile.getticket.model.dao
 
-import androidx.lifecycle.LiveData
-import androidx.paging.DataSource
 import androidx.room.*
+import io.reactivex.Maybe
+import io.reactivex.Single
 import org.pbreakers.mobile.getticket.model.entity.Bus
 
 @Dao
 interface BusDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun add(vararg data: Bus)
 
     @Query("SELECT * FROM Bus WHERE idAgence=:id")
-    fun findById(id: Int): LiveData<Bus>
+    fun findById(id: Int): Maybe<Bus>
 
     @Query("SELECT * FROM Bus")
-    fun findAll(): DataSource.Factory<Int, Bus>
+    fun findAll(): Maybe<List<Bus>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun add(vararg data: Bus) : Single<Int>
 
     @Update
-    fun update(vararg data: Bus)
+    fun update(vararg data: Bus) : Single<Int>
 
     @Delete
-    fun remove(vararg data: Bus)
+    fun remove(vararg data: Bus) : Single<Int>
 }
