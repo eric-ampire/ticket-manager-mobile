27d6ad0593f54322e7d5e21302156b3d53cdce46
Eric Ampire <ericampire.top@gmail.com>
2019-06-04 19:43:12 +0200

    [ADD] EnregViewModel.kt

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/viewmodel/EnregViewModel.kt
@@ -0,0 +1,7 @@
+package org.pbreakers.mobile.getticket.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class EnregViewModel(val app: Application) : AndroidViewModel(app) {
+}
