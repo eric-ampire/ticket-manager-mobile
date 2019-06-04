5649550073b7fe1b04b7079beacd6ba725e9ab4e
Eric Ampire <ericampire.top@gmail.com>
2019-06-04 11:26:56 +0200

    [UPDATE] Agency View interaction logic

--- app/src/main/java/org/pbreakers/mobile/getticket/view/fragment/EnregFragment.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/view/fragment/EnregFragment.kt
@@ -1,32 +1,20 @@
 package org.pbreakers.mobile.getticket.view.fragment
 
-import android.content.Context
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_enreg.*
+import kotlinx.android.synthetic.main.fragment_enreg.view.*
 import org.pbreakers.mobile.getticket.R
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import org.pbreakers.mobile.getticket.util.Tools
+import org.pbreakers.mobile.getticket.util.ViewAnimation
 
 
 class EnregFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,39 +24,59 @@ class EnregFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_enreg, container, false)
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException("$context must implement OnFragmentInteractionListener")
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
     interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         fun onFragmentInteraction(uri: Uri)
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EnregFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        // Agency
+        initAgencyComponent(view)
+    }
+
+    private fun initAgencyComponent(view: View) {
+        view.btnToggleAgency.setOnClickListener {
+            toggleSection(it, view.lytExpandAgency)
+        }
+
+        view.btnHideAgency.setOnClickListener {
+            toggleSection(view.btnToggleAgency, view.lytExpandAgency)
+        }
+
+        view.btnEnregAgency.setOnClickListener {
+            if (view.edtNomAgency.text.isBlank()) {
+                view.edtNomAgency.error = getString(R.string.input_empty)
+            } else {
+                // Todo: Save agency
             }
+        }
+    }
+
+    // The [view] represent the toggle and [layout] the layout that will be hided
+    private fun toggleSection(view: View, layout: LinearLayout) {
+
+        val show = toggleArrow(view)
+
+        if (show) {
+            ViewAnimation.expand(layout, object : ViewAnimation.AnimListener {
+                override fun onFinish() {
+                    Tools.nestedScrollTo(nestedScrollView, layout)
+                }
+            })
+
+        } else {
+            ViewAnimation.collapse(layout)
+        }
+    }
+
+    private fun toggleArrow(view: View): Boolean {
+        return if (view.rotation == 0f) {
+            view.animate().setDuration(200).rotation(180f)
+            true
+        } else {
+            view.animate().setDuration(200).rotation(0f)
+            false
+        }
     }
 }
