d738fb78de322ba6fb7c369826fc239a125140b3
Eric Ampire <ericampire.top@gmail.com>
2019-06-04 21:24:29 +0200

    [ADD] LieuRepository.kt

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/model/repository/LieuRepository.kt
@@ -0,0 +1,17 @@
+package org.pbreakers.mobile.getticket.model.repository
+
+import androidx.lifecycle.LiveData
+import org.pbreakers.mobile.getticket.model.dao.LieuDao
+import org.pbreakers.mobile.getticket.model.entity.Lieu
+import javax.inject.Inject
+
+class LieuRepository @Inject constructor(val dao: LieuDao) {
+    fun findAll(): LiveData<List<Lieu>> {
+        refresh()
+        return dao.findAll()
+    }
+
+    private fun refresh() {
+        // Todo: You have to implement this method
+    }
+}
