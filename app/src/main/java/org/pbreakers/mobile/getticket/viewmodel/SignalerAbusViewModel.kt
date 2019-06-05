260b34b822f93262e2b43904af05dd7b6e8f9d85
Eric Ampire <ericampire.top@gmail.com>
2019-06-05 20:21:39 +0200

    [UPDATE] Pass data to the view

--- app/src/main/java/org/pbreakers/mobile/getticket/viewmodel/SignalerAbusViewModel.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/viewmodel/SignalerAbusViewModel.kt
@@ -2,6 +2,23 @@ package org.pbreakers.mobile.getticket.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import org.pbreakers.mobile.getticket.app.App
+import org.pbreakers.mobile.getticket.model.entity.Bus
+import org.pbreakers.mobile.getticket.model.repository.BusRepository
+import javax.inject.Inject
 
-class SignalerAbusViewModel(val appli: Application) : AndroidViewModel(appli) {
+class SignalerAbusViewModel(val app: Application) : AndroidViewModel(app) {
+
+    @Inject lateinit var busRepository: BusRepository
+
+    val bus = MutableLiveData<List<Bus>>().apply { value = arrayListOf() }
+
+
+    init {
+        val application = app as App
+        application.appComponent.inject(this)
+
+        busRepository.findAllLiveData().observeForever { bus.value = it }
+    }
 }
