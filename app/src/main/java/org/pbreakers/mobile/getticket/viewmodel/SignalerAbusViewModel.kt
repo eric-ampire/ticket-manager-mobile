eca95637321fc21437e796f76b3c55faaad05e06
Eric Ampire <ericampire.top@gmail.com>
2019-06-05 16:35:28 +0200

    [UPDATE] SignalerAbusViewModel.kt

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/viewmodel/SignalerAbusViewModel.kt
@@ -0,0 +1,7 @@
+package org.pbreakers.mobile.getticket.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class SignalerAbusViewModel(val appli: Application) : AndroidViewModel(appli) {
+}
