3cbe1429dbc1767648a7074c711fe90bc19ae585
Eric Ampire <ericampire.top@gmail.com>
2019-06-05 19:03:29 +0200

    [UPDATE] The ViewModel Hold the View data

--- app/src/main/java/org/pbreakers/mobile/getticket/viewmodel/EnregViewModel.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/viewmodel/EnregViewModel.kt
@@ -3,6 +3,7 @@ package org.pbreakers.mobile.getticket.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import org.pbreakers.mobile.getticket.app.App
 import org.pbreakers.mobile.getticket.model.entity.*
 import org.pbreakers.mobile.getticket.model.repository.*
@@ -17,17 +18,28 @@ class EnregViewModel(val app: Application) : AndroidViewModel(app) {
     @Inject lateinit var lieuRepository: LieuRepository
     @Inject lateinit var userRepository: UtilisateurRepository
     @Inject lateinit var busRepository: BusRepository
+    @Inject lateinit var voyageRepository: VoyageRepository
+    @Inject lateinit var pointArretRepository: PointArretRepository
 
-    val role = arrayListOf<Role>()
+    val role     = MutableLiveData<List<Role>>().apply { value = arrayListOf() }
+    val agences  = MutableLiveData<List<Agence>>().apply { value = arrayListOf() }
+    val bus      = MutableLiveData<List<Bus>>().apply { value = arrayListOf() }
+    val etats    = MutableLiveData<List<Etat>>().apply { value = arrayListOf() }
+    val transits = MutableLiveData<List<Transit>>().apply { value = arrayListOf() }
+    val lieu     = MutableLiveData<List<Lieu>>().apply { value = arrayListOf() }
+    val voyages  = MutableLiveData<List<Voyage>>().apply { value = arrayListOf() }
 
     init {
         val application = app as App
         application.appComponent.inject(this)
 
-        roleRepository.findAll().observeForever {
-            role.clear()
-            role.addAll(it)
-        }
+        roleRepository.findAll().observeForever   { role.value = it }
+        agencyRepository.findAll().observeForever { agences.value = it }
+        busRepository.findAllLiveData().observeForever { bus.value = it }
+        etatRepository.findAll().observeForever { etats.value = it }
+        transitRepository.findAll().observeForever { transits.value = it }
+        lieuRepository.findAll().observeForever { lieu.value = it }
+        voyageRepository.findAllLiveData().observeForever { voyages.value = it }
     }
 
     fun findRole(): LiveData<List<Role>> {
@@ -60,5 +72,17 @@ class EnregViewModel(val app: Application) : AndroidViewModel(app) {
 
     fun saveBus(bus: Bus, function: () -> Unit) {
         busRepository.add(bus, function)
+    }
+
+    fun findAgency(): LiveData<List<Agence>> {
+        return agencyRepository.findAll()
+    }
+
+    fun saveVoyage(voyage: Voyage, function: () -> Unit) {
+        voyageRepository.add(voyage, function)
+    }
+
+    fun savePointArret(pointArret: PointArret, function: () -> Unit) {
+        pointArretRepository.add(pointArret, function)
     }
 }
