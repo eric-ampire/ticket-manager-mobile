f352c303e12408a10a10c7870464efa9e42b7824
Eric Ampire <ericampire.top@gmail.com>
2019-06-05 08:42:27 +0200

    [UPDATE] Add functionality

--- app/src/main/java/org/pbreakers/mobile/getticket/viewmodel/EnregViewModel.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/viewmodel/EnregViewModel.kt
@@ -2,6 +2,58 @@ package org.pbreakers.mobile.getticket.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import org.pbreakers.mobile.getticket.app.App
+import org.pbreakers.mobile.getticket.model.entity.*
+import org.pbreakers.mobile.getticket.model.repository.*
+import javax.inject.Inject
 
 class EnregViewModel(val app: Application) : AndroidViewModel(app) {
+
+    @Inject lateinit var roleRepository: RoleRepository
+    @Inject lateinit var agencyRepository: AgenceRepository
+    @Inject lateinit var etatRepository: EtatRepository
+    @Inject lateinit var transitRepository: TransitRepository
+    @Inject lateinit var lieuRepository: LieuRepository
+    @Inject lateinit var userRepository: UtilisateurRepository
+
+    val role = arrayListOf<Role>()
+
+    init {
+        val application = app as App
+        application.appComponent.inject(this)
+
+        roleRepository.findAll().observeForever {
+            role.clear()
+            role.addAll(it)
+        }
+    }
+
+    fun findRole(): LiveData<List<Role>> {
+        return roleRepository.findAll()
+    }
+
+    fun saveAgence(agency: Agence, function: () -> Unit) {
+        agencyRepository.add(agency, function)
+    }
+
+    fun saveEtat(etat: Etat, function: () -> Unit) {
+        etatRepository.add(etat, function)
+    }
+
+    fun saveLieu(lieu: Lieu, function: () -> Unit) {
+        lieuRepository.add(lieu, function)
+    }
+
+    fun saveRole(role: Role, function: () -> Unit) {
+        roleRepository.add(role, function)
+    }
+
+    fun saveTransit(transit: Transit, function: () -> Unit) {
+        transitRepository.add(transit, function)
+    }
+
+    fun saveUser(user: Utilisateur, function: () -> Unit) {
+        userRepository.add(user, function)
+    }
 }
