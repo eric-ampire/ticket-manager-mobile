5f0562f7a4574bf207f8a0e9c25b8aa5988c8d73
Eric Ampire <ericampire.top@gmail.com>
2019-06-07 14:22:32 +0200

    [UPDATE] Adding new query method

--- app/src/main/java/org/pbreakers/mobile/getticket/model/repository/BilletRepository.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/model/repository/BilletRepository.kt
@@ -2,6 +2,7 @@ package org.pbreakers.mobile.getticket.model.repository
 
 import androidx.lifecycle.LiveData
 import org.pbreakers.mobile.getticket.model.dao.BilletDao
+import org.pbreakers.mobile.getticket.model.entity.Billet
 import javax.inject.Inject
 
 class BilletRepository @Inject constructor(private val dao: BilletDao) {
@@ -13,5 +14,15 @@ class BilletRepository @Inject constructor(private val dao: BilletDao) {
 
     private fun refresh() {
 
+    }
+
+    fun findByIdUser(id: Long): LiveData<List<Billet>> {
+        refresh()
+        return dao.findByIdUser(id)
+    }
+
+    fun findAll(): LiveData<List<Billet>> {
+        refresh()
+        return dao.findAll()
     }
 }
