c874153b6bc2c221e9db5a38cae0deb8bf2f615c
Eric Ampire <ericampire.top@gmail.com>
2019-06-08 21:52:07 +0200

    [ADD] DetailVoyageViewModel.kt

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/viewmodel/DetailVoyageViewModel.kt
@@ -0,0 +1,7 @@
+package org.pbreakers.mobile.getticket.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class DetailVoyageViewModel(val app: Application) : AndroidViewModel(app) {
+}
