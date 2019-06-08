994d17b889b67d646ccc9f29bfe982ed555fc135
Eric Ampire <ericampire.top@gmail.com>
2019-06-08 09:48:55 +0200

    [UPDATE] New query for delete item

--- app/src/main/java/org/pbreakers/mobile/getticket/model/repository/BusRepository.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/model/repository/BusRepository.kt
@@ -2,7 +2,9 @@ package org.pbreakers.mobile.getticket.model.repository
 
 import androidx.lifecycle.LiveData
 import androidx.paging.DataSource
+import io.reactivex.Completable
 import io.reactivex.Maybe
+import io.reactivex.Single
 import org.pbreakers.mobile.getticket.model.dao.BusDao
 import org.pbreakers.mobile.getticket.model.entity.Bus
 import javax.inject.Inject
@@ -17,6 +19,11 @@ class BusRepository @Inject constructor(private val dao: BusDao) {
         return dao.findAll()
     }
 
+    fun count(): LiveData<Int> {
+        refresh()
+        return dao.count()
+    }
+
     fun findAllLiveData(): LiveData<List<Bus>> {
         refresh()
         return dao.findAllLiveData()
@@ -29,5 +36,11 @@ class BusRepository @Inject constructor(private val dao: BusDao) {
     fun add(bus: Bus, function: () -> Unit) {
         dao.add(bus)
         function()
+    }
+
+    fun remove(item: Bus): Completable {
+        // Remove in remote db
+        // Remove on local database on succes
+        return dao.remove(item)
     }
 }
