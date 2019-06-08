fc434c0b2d4aa90e9b4132568e324dea050e7ccd
Eric Ampire <ericampire.top@gmail.com>
2019-06-08 09:39:08 +0200

    [UPDATE] Refactoring method

--- app/src/main/java/org/pbreakers/mobile/getticket/adapter/OnItemClickListener.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/adapter/OnItemClickListener.kt
@@ -4,7 +4,5 @@ import android.view.View
 
 interface OnItemClickListener<I> {
     fun onClick(view: View, item: I, position: Int)
-    fun onLongClick(view: View, item: I, position: Int) {
-
-    }
+    fun onClickPopupButton(view: View, item: I, position: Int)
 }
