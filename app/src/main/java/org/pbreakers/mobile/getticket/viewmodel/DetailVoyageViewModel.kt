32bd87e4756ebadd38db5d923350a9a7ca7ca160
Eric Ampire <ericampire.top@gmail.com>
2019-06-09 08:45:36 +0200

    [UPDATE] Load all data necessary for the view

--- app/src/main/java/org/pbreakers/mobile/getticket/viewmodel/DetailVoyageViewModel.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/viewmodel/DetailVoyageViewModel.kt
@@ -1,7 +1,69 @@
 package org.pbreakers.mobile.getticket.viewmodel
 
 import android.app.Application
+import androidx.databinding.ObservableField
 import androidx.lifecycle.AndroidViewModel
+import org.pbreakers.mobile.getticket.app.App
+import org.pbreakers.mobile.getticket.model.entity.Agence
+import org.pbreakers.mobile.getticket.model.entity.Bus
+import org.pbreakers.mobile.getticket.model.entity.Voyage
+import org.pbreakers.mobile.getticket.model.repository.AgenceRepository
+import org.pbreakers.mobile.getticket.model.repository.BusRepository
+import org.pbreakers.mobile.getticket.model.repository.EtatRepository
+import org.pbreakers.mobile.getticket.model.repository.LieuRepository
+import javax.inject.Inject
 
 class DetailVoyageViewModel(val app: Application) : AndroidViewModel(app) {
+
+    @Inject lateinit var agencyRepository: AgenceRepository
+    @Inject lateinit var etatRepository: EtatRepository
+    @Inject lateinit var lieuRepository: LieuRepository
+    @Inject lateinit var busRepository: BusRepository
+
+    lateinit var voyage: Voyage
+
+    val prov   = ObservableField<String>()
+    val desti  = ObservableField<String>()
+    val etat   = ObservableField<String>()
+    val bus    = ObservableField<Bus>()
+    val agence = ObservableField<Agence>()
+
+    init {
+        val application = app as App
+        application.appComponent.inject(this)
+    }
+
+    fun init() {
+        findDestinationById(voyage.idDestination)
+        findProvenanceById(voyage.idProvenance)
+        findEtatById(voyage.idEtat)
+        findBusById(voyage.idBus)
+    }
+
+    private fun findProvenanceById(id: Long) {
+        lieuRepository.findById(id).observeForever {
+            prov.set(it.nomLieu)
+        }
+    }
+
+    private fun findDestinationById(id: Long) {
+        lieuRepository.findById(id).observeForever {
+            desti.set(it.nomLieu)
+        }
+    }
+
+    private fun findBusById(id: Long) {
+        busRepository.findById(id).observeForever { aBus ->
+            agencyRepository.findById(aBus.idAgence).observeForever { agency ->
+                bus.set(aBus)
+                agence.set(agency)
+            }
+        }
+    }
+
+    private fun findEtatById(id: Long) {
+        etatRepository.findById(id).observeForever {
+            etat.set(it.nomEtat)
+        }
+    }
 }
