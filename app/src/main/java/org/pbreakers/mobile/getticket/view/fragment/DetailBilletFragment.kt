3f34f4cf5243d5b79f0a314d46b87ebf27b238ef
Eric Ampire <ericampire.top@gmail.com>
2019-06-09 16:26:46 +0200

    [UPDATE] Connect viewModel to the fragment

--- app/src/main/java/org/pbreakers/mobile/getticket/view/fragment/DetailBilletFragment.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/view/fragment/DetailBilletFragment.kt
@@ -6,17 +6,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil.inflate
+import androidx.lifecycle.ViewModelProviders
 
 import org.pbreakers.mobile.getticket.R
+import org.pbreakers.mobile.getticket.databinding.FragmentDetailBilletBinding
+import org.pbreakers.mobile.getticket.model.entity.Billet
+import org.pbreakers.mobile.getticket.viewmodel.DetailBilletViewModel
 
 
 class DetailBilletFragment : Fragment() {
+
+    private val currentBillet by lazy {
+        arguments?.getParcelable<Billet>("billet")
+    }
+
+    private val detailBilletViewModel by lazy {
+        ViewModelProviders.of(this).get(DetailBilletViewModel::class.java).apply {
+            billet = currentBillet!!
+            init()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_billet, container, false)
+        val binding by lazy {
+            inflate<FragmentDetailBilletBinding>(inflater, R.layout.fragment_detail_billet, container, false).apply {
+                viewModel = detailBilletViewModel
+            }
+        }
+
+        return binding.root
     }
 }
