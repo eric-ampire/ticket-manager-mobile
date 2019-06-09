94b7257c3d30a52f04cbedf7b296139545d863ed
Eric Ampire <ericampire.top@gmail.com>
2019-06-09 08:51:23 +0200

    [UPDATE] Bind data with the view

--- app/src/main/java/org/pbreakers/mobile/getticket/view/fragment/DetailVoyageFragment.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/view/fragment/DetailVoyageFragment.kt
@@ -5,27 +5,44 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.TextView
-import androidx.core.widget.NestedScrollView
+import androidx.databinding.DataBindingUtil.inflate
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.fragment_voyage_detail.*
 import org.pbreakers.mobile.getticket.R
-import org.pbreakers.mobile.getticket.util.Tools
-import org.pbreakers.mobile.getticket.util.Tools.toggleArrow
+import org.pbreakers.mobile.getticket.databinding.FragmentVoyageDetailBinding
+import org.pbreakers.mobile.getticket.model.entity.Voyage
 import org.pbreakers.mobile.getticket.util.Tools.toggleSection
-import org.pbreakers.mobile.getticket.util.ViewAnimation
+import org.pbreakers.mobile.getticket.viewmodel.DetailVoyageViewModel
 
 
 class DetailVoyageFragment : Fragment() {
+
+    // Todo: You have to use the constant
+    private val currentVoyage by lazy {
+        arguments?.getParcelable<Voyage>("voyage")
+    }
+
+    private val detailVoyageViewModel by lazy {
+        ViewModelProviders.of(this).get(DetailVoyageViewModel::class.java).apply {
+            voyage = currentVoyage!!
+            init()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_voyage_detail, container, false)
+        val binding = inflate<FragmentVoyageDetailBinding>(
+            inflater, R.layout.fragment_voyage_detail, container, false).apply {
+
+            voyage = currentVoyage
+            viewModel = detailVoyageViewModel
+        }
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -35,19 +52,12 @@ class DetailVoyageFragment : Fragment() {
 
     private fun initComponent() {
         // info item_section
-        btnToggleInfo.setOnClickListener {
-            toggleSection(btnToggleInfo, lytExpandInfo, nestedScrollView)
-        }
-        btnHideInfo.setOnClickListener {
-            toggleSection(btnToggleInfo, lytExpandInfo, nestedScrollView)
-        }
+        btnToggleInfo.setOnClickListener { toggleSection(btnToggleInfo, lytExpandInfo, nestedScrollView) }
+        btnHideInfo.setOnClickListener { toggleSection(btnToggleInfo, lytExpandInfo, nestedScrollView) }
 
         // passenger item_section
-        btnTogglePassenger.setOnClickListener {
-            toggleSection(btnTogglePassenger, lytExpandPassenger, nestedScrollView)
-        }
+        btnTogglePassenger.setOnClickListener { toggleSection(btnTogglePassenger, lytExpandPassenger, nestedScrollView) }
 
-        // copy to clipboard
         btnReserve.setOnClickListener {
             // reservation
         }
