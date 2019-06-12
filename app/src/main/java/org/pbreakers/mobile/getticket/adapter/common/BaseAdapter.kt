92d6c6410b2fc4af0cb2ee6c9ddd02ccb6d7de4e
Eric Ampire <ericampire.top@gmail.com>
2019-06-12 20:01:09 +0200

    [ADD] BaseAdapter.kt

---
+++ app/src/main/java/org/pbreakers/mobile/getticket/adapter/common/BaseAdapter.kt
@@ -0,0 +1,20 @@
+package org.pbreakers.mobile.getticket.adapter.common
+
+import android.view.View
+import androidx.databinding.ObservableInt
+import androidx.paging.PagedList
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import org.pbreakers.mobile.getticket.adapter.CustomViewHolder
+
+abstract class BaseAdapter<T>(diff: DiffUtil.ItemCallback<T>) : PagedListAdapter<T, CustomViewHolder>(diff) {
+    val emptyViewVisibility = ObservableInt(View.VISIBLE)
+
+    override fun onCurrentListChanged(currentList: PagedList<T>?) {
+        if (itemCount == 0) {
+            emptyViewVisibility.set(View.VISIBLE)
+        } else {
+            emptyViewVisibility.set(View.GONE)
+        }
+    }
+}
