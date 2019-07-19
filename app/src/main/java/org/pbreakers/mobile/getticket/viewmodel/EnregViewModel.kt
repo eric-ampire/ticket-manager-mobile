15e314addc2f12a93935519d14a608e72510008d
Eric Ampire <ericampire.top@gmail.com>
2019-07-20 01:26:33 +0200

    [UPDATE] Refactoring

--- app/src/main/java/org/pbreakers/mobile/getticket/viewmodel/EnregViewModel.kt
+++ app/src/main/java/org/pbreakers/mobile/getticket/viewmodel/EnregViewModel.kt
@@ -3,14 +3,29 @@ package org.pbreakers.mobile.getticket.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.*
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 import org.pbreakers.mobile.getticket.model.entity.*
+import org.pbreakers.mobile.getticket.util.LoadingState
+import org.pbreakers.mobile.getticket.util.Session
 
-class EnregViewModel : ViewModel() {
+class EnregViewModel : ViewModel(), KoinComponent {
+
+    private val session: Session by inject()
 
     private val db by lazy {
         FirebaseFirestore.getInstance()
     }
+
+    private val auth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
+    private val _loadingState = MutableLiveData<LoadingState>()
+    val loadingState: LiveData<LoadingState>
+        get() = _loadingState
 
     val role: LiveData<List<Role>>
         get() = _role
@@ -77,6 +92,57 @@ class EnregViewModel : ViewModel() {
         db.collection("voyages").addSnapshotListener { snapshot, exception ->
             if (exception != null || snapshot == null) return@addSnapshotListener
             _voyages.postValue(snapshot.toObjects(Voyage::class.java))
+        }
+
+
+    }
+
+    fun createUser(user: Utilisateur) {
+
+        _loadingState.value = LoadingState.RUNNING
+        val authTask = auth.createUserWithEmailAndPassword(user.pseudoUtilisateur, user.passwordUtilisateur)
+        authTask.addOnCompleteListener { registrationTask ->
+            if (registrationTask.isSuccessful) {
+                user.idUtilisateur = auth.currentUser!!.uid
+                val task = db.collection("users").document(user.idUtilisateur).set(user)
+                task.addOnCompleteListener { savingTask ->
+                    if (savingTask.isSuccessful) {
+
+                        // Logout with this user
+                        FirebaseAuth.getInstance().signOut()
+
+                        // Login with old user
+                        signingWithOldCredential()
+
+                    } else {
+                        // Change loading status
+                        _loadingState.value = LoadingState.error(savingTask.exception?.message)
+                    }
+                }
+
+            } else {
+                // Change loading status
+                _loadingState.value = LoadingState.error(registrationTask.exception?.message)
+            }
+        }
+    }
+
+    private fun signingWithOldCredential() {
+
+        val oldUser = session.getCurrentUser()
+
+        if (oldUser == null) {
+            _loadingState.value = LoadingState.error("Probleme d'authentification")
+        } else {
+            val authTask = auth.signInWithEmailAndPassword(oldUser.pseudoUtilisateur, oldUser.passwordUtilisateur)
+            authTask.addOnCompleteListener {
+                if (it.isSuccessful) {
+                    // Change loading status
+                    _loadingState.value = LoadingState.LOADED
+                } else {
+                    _loadingState.value = LoadingState.error(it.exception?.message)
+                }
+            }
         }
     }
 }
